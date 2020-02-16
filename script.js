e8a2f028a718b282e3b3a692a864b4d14831a13a
kryha5555 <kryha5555@gmail.com>
2020-02-16 23:20:58 +0100

    it works

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+let init = () => {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            jsonObj = JSON.parse(this.responseText);
+            console.log(jsonObj);
+            wypiszMenu();
+        }
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+    document.title = "Wybierz instrukcję";
+}
+
 let wypiszMenu = () => {
     let menuRoot = document.getElementById("interface");
     while (menuRoot.firstChild)
         menuRoot.removeChild(menuRoot.firstChild);
-
-    //menuRoot.innerHTML = "<ul><div class=\"menuheader\">Wybierz instrukcję</div></ul>";
 
     let ul = document.createElement("ul");
     let menuheader = document.createElement("div");
@@ -13,15 +25,12 @@ let wypiszMenu = () => {
     menuRoot.appendChild(ul);
 
     for (instr in jsonObj.instrukcja) {
-        //console.log(jsonObj.instrukcja[instr]);
-        let li = document.createElement("li")
         let mins = document.createElement("div");
         mins.className = "minstr";
         mins.setAttribute("onclick", "wypiszInstrukcje(" + instr + ")");
-        li.appendChild(mins)
-
         mins.innerHTML = jsonObj.instrukcja[instr].numer + ". " + jsonObj.instrukcja[instr].temat;
-
+        let li = document.createElement("li")
+        li.appendChild(mins)
         ul.appendChild(li);
     }
 
@@ -29,24 +38,27 @@ let wypiszMenu = () => {
 
 }
 let wypiszInstrukcje = (instr) => {
+
+    let czyZwin = document.getElementsByTagName("li")[instr].getElementsByClassName("mpunkt").length;
     wypiszMenu();
     let li = document.getElementsByTagName("li")[instr];
 
+    document.title = jsonObj.instrukcja[instr].temat;
+    document.getElementsByClassName("minstr")[instr].className += " active";
+
     for (p in jsonObj.instrukcja[instr].punkt) {
-        console.log(jsonObj.instrukcja[instr].punkt[p]);
         if (jsonObj.instrukcja[instr].punkt[p].tytul !== "") {
             let mpunkt = document.createElement("div");
             mpunkt.className = "mpunkt";
+            mpunkt.setAttribute("onclick", "wypiszPunkt(" + instr + "," + p + ")");
             li.appendChild(mpunkt);
-            mpunkt.innerHTML = jsonObj.instrukcja[instr].punkt[p].tytul;
+            mpunkt.innerHTML = parseInt(p) + 1 + ". " + jsonObj.instrukcja[instr].punkt[p].tytul;
         }
     }
-
 
     let instrukcjaRoot = document.getElementById("instrukcja");
     while (instrukcjaRoot.firstChild)
         instrukcjaRoot.removeChild(instrukcjaRoot.firstChild);
-    //var instr = document.getElementById("wybor").value;
 
     let numer = document.createElement("div");
     numer.className = "numer";
@@ -65,65 +77,73 @@ let wypiszInstrukcje = (instr) => {
         specyfikacja.innerHTML = jsonObj.instrukcja[instr].specyfikacja[sp].opis + ": <a href=\"" + jsonObj.instrukcja[instr].specyfikacja[sp].link + "\" target=\"_blank\">" + jsonObj.instrukcja[instr].specyfikacja[sp].link + "</a><br/>";
     }
 
-    for (p in jsonObj.instrukcja[instr].punkt) {
-        let punkt = document.createElement("div");
-        punkt.className = "punkt";
-        instrukcja.appendChild(punkt);
-
-        let tytul = document.createElement("div");
-        tytul.className = "tytul";
-        punkt.appendChild(tytul);
-        tytul.innerHTML = jsonObj.instrukcja[instr].punkt[p].tytul;
-
-        let tekst = document.createElement("div");
-        tekst.className = "tekst";
-        punkt.appendChild(tekst);
-        tekst.innerHTML = jsonObj.instrukcja[instr].punkt[p].tekst;
-
-        for (pp in jsonObj.instrukcja[instr].punkt[p].podpunkt) {
-            let podpunkt = document.createElement("div");
-            podpunkt.className = "podpunkt";
-            punkt.appendChild(podpunkt);
-
-            let podtytul = document.createElement("div");
-            podtytul.className = "podtytul";
-            podpunkt.appendChild(podtytul);
-            podtytul.innerHTML = jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].tytul;
-
-            if (jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].kod !== "") {
-                let kod = document.createElement("div");
-                kod.className = "kod";
-                podpunkt.appendChild(kod);
-                kod.innerHTML = "<table><td><pre><code>" + jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].kod + "</code></pre></td></table>";
-            }
-
-            let podpis = document.createElement("div");
-            podpis.className = "podpis";
-            podpunkt.appendChild(podpis);
-            podpis.innerHTML = jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].podpis;
-
-            if (jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].obraz !== "") {
-                let obraz = document.createElement("div");
-                obraz.className = "obraz";
-                podpunkt.appendChild(obraz);
-                obraz.innerHTML = "<img src=" + jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].obraz + " width='100' height='100'></img>";
-            }
-        }
+    if (czyZwin) {
+        while (li.childElementCount > 1)
+            li.removeChild(li.lastChild);
     }
 }
+let wypiszPunkt = (instr, p) => {
+    document.title = jsonObj.instrukcja[instr].temat + " - " + jsonObj.instrukcja[instr].punkt[p].tytul;
+    document.getElementsByClassName("minstr")[instr].className = "minstr";
+    for (pk in jsonObj.instrukcja[instr].punkt) {
+        if (pk == p)
+            document.getElementsByClassName("mpunkt")[pk].className = "mpunkt active";
+        else
+            document.getElementsByClassName("mpunkt")[pk].className = "mpunkt"
+    }
 
+    let instrukcjaRoot = document.getElementById("instrukcja");
+    while (instrukcjaRoot.firstChild)
+        instrukcjaRoot.removeChild(instrukcjaRoot.firstChild);
 
-addEventListener('load', wypiszMenu);
-var xmlhttp = new XMLHttpRequest();
+    instrukcjaRoot.scrollTo({ top: 0, behavior: 'smooth' });
+
+    let punkt = document.createElement("div");
+    punkt.className = "punkt";
+    instrukcja.appendChild(punkt);
+
+    let tytul = document.createElement("div");
+    tytul.className = "tytul";
+    punkt.appendChild(tytul);
+    tytul.innerHTML = jsonObj.instrukcja[instr].punkt[p].tytul;
+
+    let tekst = document.createElement("div");
+    tekst.className = "tekst";
+    punkt.appendChild(tekst);
+    tekst.innerHTML = jsonObj.instrukcja[instr].punkt[p].tekst;
+
+    for (pp in jsonObj.instrukcja[instr].punkt[p].podpunkt) {
+        let podpunkt = document.createElement("div");
+        podpunkt.className = "podpunkt";
+        punkt.appendChild(podpunkt);
+
+        let podtytul = document.createElement("div");
+        podtytul.className = "podtytul";
+        podpunkt.appendChild(podtytul);
+        podtytul.innerHTML = jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].tytul;
+
+        if (jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].kod !== "") {
+            let kod = document.createElement("div");
+            kod.className = "kod";
+            podpunkt.appendChild(kod);
+            kod.innerHTML = "<table><td><pre><code>" + jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].kod + "</code></pre></td></table>";
+        }
+
+        let podpis = document.createElement("div");
+        podpis.className = "podpis";
+        podpunkt.appendChild(podpis);
+        podpis.innerHTML = jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].podpis;
+
+        if (jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].obraz !== "") {
+            let obraz = document.createElement("div");
+            obraz.className = "obraz";
+            podpunkt.appendChild(obraz);
+            obraz.innerHTML = "<img src=" + jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].obraz + " width='100' height='100'></img>";
+        }
+    }
+
+}
 var url = "instr.json";
 var jsonObj;
 
-xmlhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200)
-        jsonObj = JSON.parse(this.responseText);
-}
-
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
-
-//todo dodać ify jak przy kodzie i obrazie
+window.addEventListener('load', init);
