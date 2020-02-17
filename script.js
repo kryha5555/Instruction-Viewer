b567734a67caa95237d7db3b9c8e927217e573c9
kryha5555 <kryha5555@gmail.com>
2020-02-17 20:14:08 +0100

    finished

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let init = () => {
     }
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
-    document.title = "Wybierz instrukcję";
+    document.title = "Technologie sieci WEB - instrukcje do laboratorium";
 }
 
 let wypiszMenu = () => {
@@ -52,7 +52,7 @@ let wypiszInstrukcje = (instr) => {
             mpunkt.className = "mpunkt";
             mpunkt.setAttribute("onclick", "wypiszPunkt(" + instr + "," + p + ")");
             li.appendChild(mpunkt);
-            mpunkt.innerHTML = parseInt(p) + 1 + ". " + jsonObj.instrukcja[instr].punkt[p].tytul;
+            mpunkt.innerHTML = (parseInt(p) + 1) + ". " + jsonObj.instrukcja[instr].punkt[p].tytul;
         }
     }
 
@@ -63,18 +63,22 @@ let wypiszInstrukcje = (instr) => {
     let numer = document.createElement("div");
     numer.className = "numer";
     instrukcjaRoot.appendChild(numer);
-    numer.innerHTML = "Ćwiczenie nr " + jsonObj.instrukcja[instr].numer;
+    numer.innerHTML = "<h1>Ćwiczenie nr " + jsonObj.instrukcja[instr].numer + "</h1>";
 
     let temat = document.createElement("div");
     temat.className = "temat";
     instrukcjaRoot.appendChild(temat);
-    temat.innerHTML = "Temat: " + jsonObj.instrukcja[instr].temat;
+    //temat.innerHTML = "Temat: " + jsonObj.instrukcja[instr].temat;
+    temat.innerHTML = "<h2>" + jsonObj.instrukcja[instr].temat + "</h2>";
+
+    let ul = document.createElement("ul");
+    instrukcja.appendChild(ul);
 
     for (sp in jsonObj.instrukcja[instr].specyfikacja) {
         let specyfikacja = document.createElement("div");
         specyfikacja.className = "specyfikacja";
-        instrukcja.appendChild(specyfikacja);
-        specyfikacja.innerHTML = jsonObj.instrukcja[instr].specyfikacja[sp].opis + ": <a href=\"" + jsonObj.instrukcja[instr].specyfikacja[sp].link + "\" target=\"_blank\">" + jsonObj.instrukcja[instr].specyfikacja[sp].link + "</a><br/>";
+        ul.appendChild(specyfikacja);
+        specyfikacja.innerHTML = "<li>" + jsonObj.instrukcja[instr].specyfikacja[sp].opis + ": <a href=\"" + jsonObj.instrukcja[instr].specyfikacja[sp].link + "\" target=\"_blank\">" + jsonObj.instrukcja[instr].specyfikacja[sp].link + "</a></li>";
     }
 
     if (czyZwin) {
@@ -105,40 +109,42 @@ let wypiszPunkt = (instr, p) => {
     let tytul = document.createElement("div");
     tytul.className = "tytul";
     punkt.appendChild(tytul);
-    tytul.innerHTML = jsonObj.instrukcja[instr].punkt[p].tytul;
+    tytul.innerHTML = "<h1>" + jsonObj.instrukcja[instr].punkt[p].tytul + "</h1>";
 
     let tekst = document.createElement("div");
     tekst.className = "tekst";
     punkt.appendChild(tekst);
-    tekst.innerHTML = jsonObj.instrukcja[instr].punkt[p].tekst;
+    tekst.innerHTML = "<p>" + jsonObj.instrukcja[instr].punkt[p].tekst + "</p>";
 
     for (pp in jsonObj.instrukcja[instr].punkt[p].podpunkt) {
-        let podpunkt = document.createElement("div");
-        podpunkt.className = "podpunkt";
-        punkt.appendChild(podpunkt);
+        if (jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].tytul !== "") {
+            let podpunkt = document.createElement("div");
+            podpunkt.className = "podpunkt";
+            punkt.appendChild(podpunkt);
 
-        let podtytul = document.createElement("div");
-        podtytul.className = "podtytul";
-        podpunkt.appendChild(podtytul);
-        podtytul.innerHTML = jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].tytul;
+            let podtytul = document.createElement("div");
+            podtytul.className = "podtytul";
+            podpunkt.appendChild(podtytul);
+            podtytul.innerHTML = "<p>" + (parseInt(pp) + 1) + ". " + jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].tytul + "</p>";
 
-        if (jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].kod !== "") {
-            let kod = document.createElement("div");
-            kod.className = "kod";
-            podpunkt.appendChild(kod);
-            kod.innerHTML = "<table><td><pre><code>" + jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].kod + "</code></pre></td></table>";
-        }
+            if (jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].kod !== "") {
+                let kod = document.createElement("div");
+                kod.className = "kod";
+                podpunkt.appendChild(kod);
+                kod.innerHTML = "<table><td><pre><code>" + jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].kod + "</code></pre></td></table>";
+            }
 
-        let podpis = document.createElement("div");
-        podpis.className = "podpis";
-        podpunkt.appendChild(podpis);
-        podpis.innerHTML = jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].podpis;
+            let podpis = document.createElement("div");
+            podpis.className = "podpis";
+            podpunkt.appendChild(podpis);
+            podpis.innerHTML = jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].podpis;
 
-        if (jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].obraz !== "") {
-            let obraz = document.createElement("div");
-            obraz.className = "obraz";
-            podpunkt.appendChild(obraz);
-            obraz.innerHTML = "<img src=" + jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].obraz + " width='100' height='100'></img>";
+            if (jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].obraz !== "") {
+                let obraz = document.createElement("div");
+                obraz.className = "obraz";
+                podpunkt.appendChild(obraz);
+                obraz.innerHTML = "<img src=" + jsonObj.instrukcja[instr].punkt[p].podpunkt[pp].obraz + "></img>";
+            }
         }
     }
 
